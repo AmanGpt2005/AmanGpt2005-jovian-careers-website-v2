c775288338e311ea7a0930bbf318fb56e704342a
AmanGpt2005 <amangpt4819@gmail.com>
2025-04-09 12:50:44 +0000

    added connection to database

--- database.py
+++ database.py
@@ -1,6 +1,16 @@
 import sqlalchemy
+import os
 from sqlalchemy import create_engine,text
-engine=create_engine("mysql+pymysql://sql12770857:ad4MXKybts@sql12.freesqldatabase.com/sql12770857?charset=utf8mb4")
+
+db_connection_string= os.environ['DB_CONNECTION_STRING']
+
+engine=create_engine(
+  db_connection_string,
+  connect_args={
+    "ssl":{
+      "ssl_ca":"etc/ssl/cert.pem"
+    }
+  })
 
 with engine.connect() as conn:
   result = conn.execute(text("select * from jobs"))
